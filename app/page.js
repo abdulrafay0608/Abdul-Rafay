68e2e7c1c5f572578d99c9cf3929e350b30a5f91
abdulrafay0608 <abdulrafay0608@gmail.com>
2024-02-01 17:07:42 +0500

    set

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-import About from "@/components/About";
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import Skills from "@/components/Skills";
+import About from "@/components/About.jsx";
+import Hero from "@/components/Hero.jsx";
+import Services from "@/components/Services.jsx";
+import Skills from "@/components/Skills.jsx";
 
 // "use client"
 export default function Home() {
