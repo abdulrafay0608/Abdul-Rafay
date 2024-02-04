282845355271482be422e45f76902b066430c93e
abdulrafay0608 <abdulrafay0608@gmail.com>
2024-02-04 07:03:06 +0500

    bluid

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -30,7 +30,6 @@ const Contact = () => {
             <div className="mt-12 pb-10">
                 <h2 className="text-center text-5xl font-extrabold">Get in Touch</h2>
                 <p className="text-center font-mono pb-12">&quot;Let's Turn Your Ideas into Reality&quot;</p>
-
                 <div className="block md:flex justify-center items-center md:gap-4">
                     <div className=' bg-slate-900 rounded-md p-4 mx-2 md:p-10'>
                         <h1 className='font-bold text-2xl'><span className='border-b-2 border-yellow-400'>SAY SOM</span>ETHING</h1>
